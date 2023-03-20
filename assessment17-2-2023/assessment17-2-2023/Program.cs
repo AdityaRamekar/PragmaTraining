6bd1fe62c3988db730864366a439ea8cd65be200
AdityaRamekar <78126990+AdityaRamekar@users.noreply.github.com>
2023-03-20 09:54:24 +0530

    Add files via upload

---
+++ assessment17-2-2023/assessment17-2-2023/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assessment17_2_2023
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 65;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+               for (int j = 0; j < 5; j++)
+                {
+                    if (j - i <= 0)
+                    {
+                        Console.Write((char)c + "");
+
+                        c++;
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
