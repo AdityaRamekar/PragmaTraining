fbde7dffe0ccc47b985c3445ee137c4d638f1c79
AdityaRamekar <78126990+AdityaRamekar@users.noreply.github.com>
2023-03-20 10:29:21 +0530

    Add files via upload

---
+++ staticconstructor/staticconstructor/DefaultConstructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticconstructor
+{
+    class DefaultConstructor
+    {//default constructor is a special method which is called automatically when new instance is created
+        int i = 0;
+
+        public static void Main1()
+        {
+            DefaultConstructor d = new DefaultConstructor();
+            Console.WriteLine("default constructor = " + d.i);
+        }
+    }
+}
---
+++ staticconstructor/staticconstructor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticconstructor
+{
+    class Program
+    {
+        public static int bal = 0;
+        static DateTime time;
+        static String str;
+        static void Main(string[] args)
+        {
+            DefaultConstructor.Main1();
+            Child p = new Child();
+            Child p1 = new Child();
+            Console.WriteLine(bal);
+            Console.WriteLine(bal);
+            Console.WriteLine(bal);
+            Console.WriteLine(bal);
+
+            Console.WriteLine(time.ToString());
+            
+            Console.WriteLine(str == null?"str == null":str);
+        }
+      
+    }
+    class Child : Program
+    {//static constructor is called only once,its initialized value is available to everyone
+        static Child()
+        {
+            bal += 2;
+        }
+        public Child()
+        {
+            bal += 2;
+        }
+    }
+}
