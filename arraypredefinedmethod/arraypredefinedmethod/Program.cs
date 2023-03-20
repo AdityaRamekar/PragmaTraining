5891b0e21e86268e3b700480af04f91a8b1a76a2
AdityaRamekar <78126990+AdityaRamekar@users.noreply.github.com>
2023-03-20 09:53:00 +0530

    Add files via upload

---
+++ arraypredefinedmethod/arraypredefinedmethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraypredefinedmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //predefined array methods
+            /*1)IndexOf()
+              2)Exists()
+              3)Find()
+              4)FindLast()
+              5)FindIndex()
+              6)FindAll()
+              7)reverse()
+              8)Copy()
+              8)Sort()
+              9)BinarySearch()
+              10)Icomparer()*/
+
+            Ic
+        }
+    }
+}
