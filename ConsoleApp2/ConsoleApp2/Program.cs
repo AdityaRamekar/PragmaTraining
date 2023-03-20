e683594b98c139cad6b7d3266ef161c81ab0b8d7
AdityaRamekar <78126990+AdityaRamekar@users.noreply.github.com>
2023-03-20 09:56:25 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            
+            bool cont = true;
+            Console.WriteLine("Enter 1st no.");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd no.");
+            b = Convert.ToInt32(Console.ReadLine());
+            while (cont)
+            {
+               
+                Console.WriteLine("Enter any of the +,-,*,/ operations,type 'no' to exit");
+              
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        Console.WriteLine(a + "+" + b + "=" + (a + b));
+                        break;
+                    case "-":
+                        Console.WriteLine(a + "-" + b + "=" + (a - b));
+                        break;
+                    case "*":
+                        Console.WriteLine(a + "*" + b + "=" + (a * b));
+                        break;
+                    case "/":
+                        Console.WriteLine(a + "/" + b + "=" + (a / b));
+                        break;
+                    case "no":
+                        cont = false;
+                        Console.WriteLine("exiting..");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong operation");
+                        break;
+                }
+             
+
+            }
+        }
+    }
+}
